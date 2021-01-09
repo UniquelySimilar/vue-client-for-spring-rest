b8b12849d5fc6e1d134e0fd1f3f356aa7a50bf7c
UniquelySimilar <consciousexplorer@gmail.com>
2021-01-08 17:08:25 -0700

    added pagination component to product index view

--- src/views/product/ProductIndex.vue
+++ src/views/product/ProductIndex.vue
@@ -3,6 +3,9 @@
     <div>
       <span class="component-heading">Product List</span>
     </div>
+    <div class="product-type-filter-container">
+      <!-- product-type-filter /-->
+    </div>
     <table class="table table-striped table-bordered">
       <thead>
         <tr>
@@ -13,7 +16,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="product in products" :key="product.id">
+        <tr v-for="product in currentPageProducts" :key="product.id">
           <td>{{ product.name }}</td>
           <td>{{ product.description }}</td>
           <td>{{ product.unitPrice }}</td>
@@ -21,23 +24,54 @@
         </tr>
       </tbody>
     </table>
+
+    <pagination-control
+      @goToFirstPageEvt="goToFirstPage"
+      @incrementPageEvt="incrementPage"
+      @decrementPageEvt="decrementPage"
+      @goToLastPageEvt="goToLastPage"
+      />
   </div>
 </template>
 
 <script>
+  import PaginationControl from '@/components/PaginationControl'
+  //import ProductTypeFilter from '@/components/ProductTypeFilter'
+
   import { productRestUrl, axios, processAjaxAuthError } from '@/globalvars.js'
   export default {
+    components: {
+      PaginationControl
+      //ProductTypeFilter
+    },
     data() {
       return {
-        products: []
+        products: [],
+        //filteredProducts: [],
+        pageSize: 10,
+        currentPage: 1
       }
     },
     computed: {
       token() {
         return this.$store.state.token;
+      },
+      currentPageProducts() {
+        let startIndex = (this.currentPage - 1) * this.pageSize;
+        let endIndex = this.currentPage * this.pageSize;
+        return this.filteredProducts.slice(startIndex, endIndex);
+      },
+      pageCount() {
+        return Math.ceil(this.filteredProducts.length / this.pageSize);
+      },
+      // TEMPORARY: Until ProductTypeFilter added
+      filteredProducts() {
+        return this.products;
       }
     },
     methods: {
+      // TODO: Create 'getProductTypes()'
+      // See https://github.com/axios/axios - Performing multiple concurrent requests 
       getProducts() {
         axios.get(productRestUrl, {
           headers: {
@@ -46,8 +80,22 @@
         })
         .then( response => this.products = response.data )
         .catch( error => processAjaxAuthError(error, this.$router) );
-
-        console.log(this.products);
+      },
+      goToFirstPage() {
+        this.currentPage = 1;
+      },
+      incrementPage() {
+        if (this.currentPage < this.pageCount) {
+          this.currentPage++;
+        }
+      },
+      decrementPage() {
+        if (this.currentPage > 1) {
+          this.currentPage--;
+        }
+      },
+      goToLastPage() {
+        this.currentPage = this.pageCount;
       }
     },
     created() {
@@ -57,5 +105,8 @@
 </script>
 
 <style scoped>
+  .product-type-filter-container {
+    float: right;
+  }
 
 </style>
